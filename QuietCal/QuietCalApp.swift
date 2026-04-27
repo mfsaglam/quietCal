7e1edf26ff8ac776cf59ea19c34b7144b7ef089f
Fatih Saglam <m.saglam@enbw.com>
2026-04-27 20:37:25 +0300

    Initial Commit

---
+++ QuietCal/QuietCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuietCalApp.swift
+//  QuietCal
+//
+//  Created by Saglam, Fatih on 27.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct QuietCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
